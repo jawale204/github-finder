c95c1c60c6161dd0a489a79aa336ae58bee30d94
jawale204 <jawale204@gmail.com>
2022-04-14 22:17:18 +0530

    10-63

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -112,7 +112,7 @@ function User() {
           </div>
         </div>
 
-        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats">
+        <div className="w-full py-5 mb-6 rounded-lg shadow-md bg-base-200 stats">
           <div className="grid grid-cols-1 md:grid-cols-4">
             <div className="stat">
               <div className="stat-figure text-secondary">
